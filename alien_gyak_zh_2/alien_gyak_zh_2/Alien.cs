6b36501ddbcc01e68c37198195db273553fde2c1
Miklos Sipos <sipos.miklos@nik.uni-obuda.hu>
2019-08-29 21:30:19 +0200

    added ALIEN exercise code and test paper

---
+++ alien_gyak_zh_2/alien_gyak_zh_2/Alien.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alien_gyak_zh_2
+{
+    class Alien
+    {
+        public string ID { get; private set; }
+        public string Tipus { get; set; }
+        public DateTime KikelesIdeje { get; set; }
+        public int XPoz { get; set; }
+        public int YPoz { get; set; }
+        public string[] EmberAzonositok { get; set; }
+        public Ember[] MegoltEmberek { get; set; }
+
+        public Alien(string tipus, int x, int y, DateTime kikelesIdeje)
+        {
+            this.Tipus = tipus;
+            this.XPoz = x;
+            this.YPoz = y;
+            this.KikelesIdeje = kikelesIdeje;
+            MegoltEmberek = new Ember[7];
+            this.ID = IDGeneral();
+        }
+
+        private string IDGeneral()
+        {
+            string ev = KikelesIdeje.Year.ToString();
+            string honap = KikelesIdeje.Month.ToString();
+            string nap = KikelesIdeje.Day.ToString();
+            int sum = 0;
+
+            for (int i = 0; i < ev.Length; i++)
+                sum += int.Parse(ev[i].ToString());
+
+            for (int i = 0; i < honap.Length; i++)
+                sum += int.Parse(honap[i].ToString());
+
+            for (int i = 0; i < nap.Length; i++)
+                sum += int.Parse(nap[i].ToString());
+
+            return
+                sum.ToString() + "-" +
+                Tipus[0].ToString().ToUpper() + "-" +
+                Tipus[1].ToString().ToUpper();
+        }
+
+        public char Jelolo()
+        {
+            return char.ToUpper(this.Tipus[0]);
+        }
+
+    }
+}
---
+++ alien_gyak_zh_2/alien_gyak_zh_2/Ember.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alien_gyak_zh_2
+{
+    enum Nem
+    {
+        Ferfi = 0, No = 1
+    }
+
+    class Ember
+    {
+        private string Id;
+        public string ID
+        {
+            get { return Id.Split('#')[1]; }
+            set { Id = "#" + value; }
+        }
+        public string Nev { get; set; }
+        public DateTime FelszallasIdeje { get; set; }
+        public Nem Nem { get; set; }
+        public bool EletbenVan { get; set; }
+
+        public Ember(string nev, string id, DateTime felszallas, Nem nem, bool eletben)
+        {
+            this.EletbenVan = eletben;
+            this.Nem = nem;
+            this.FelszallasIdeje = felszallas;
+            this.Nev = nev;
+            this.ID = id;
+        }
+
+        public Ember(string fullBemenet)
+        {
+            // 5929:PETER:2030-9-7:1:1
+
+            string[] adatok = fullBemenet.Split(':');
+
+            this.ID = adatok[0];
+            this.Nev = adatok[1];
+            this.FelszallasIdeje = DateTime.Parse(adatok[2]);
+
+            if (adatok[3] == "1")
+                this.Nem = Nem.Ferfi;
+            else
+                this.Nem = Nem.No;
+
+            if (adatok[4] == "1")
+                this.EletbenVan = true;
+            else
+                this.EletbenVan = false;
+        }
+    }
+}
